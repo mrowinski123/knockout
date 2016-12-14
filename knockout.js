6ee13ada0935cd7267ca758a5a7589275011e401
Maciej Rowiński <mrowinski@it-dev.pl>
2016-12-14 10:31:09 +0100

    small changes

--- knockout.js
+++ knockout.js
@@ -1,16 +1,12 @@
 /**
- * Created by mrowinski on 09.12.2016.
+ * Created by mrowinski on 14.12.2016.
  */
-/*/**
- * Created by mrowinski on 05.12.2016.
- */
-
 
 function AppViewModel() {
-    this.firstName = ko.observable("Bert");
-    this.lastName = ko.observable("Bertington");
-    this.fullName = ko.computed(function() {
-        return this.firstName() + " " + this.lastName();
+    this.firstName = ko.observable("Donald");
+    this.lastName = ko.observable("Trump");
+    this.fullText = ko.computed(function() {
+        return "Hello, " + this.firstName() + " " + this.lastName();
     }, this);
 }
 function onLoad()
