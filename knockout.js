c1053ac2690eb9f184085423027eba6f679e38ff
Maciej Rowiński <mrowinski@it-dev.pl>
2016-12-15 10:57:57 +0100

    refactored ko.js

--- knockout.js
+++ knockout.js
@@ -1,18 +1,21 @@
 var SimpleListModel = function(items) {
+
     var person={
         id: "",
         name: "JohnDoe"
     }
-    this.items = ko.observableArray(items);
-    this.itemToAdd = ko.observable("");
-    this.addItem = function() {
-        if (this.itemToAdd() != "") {
-            this.items([]);//Clears the list of persons
-            for(var i=0; i<this.itemToAdd();i++){
+
+    this.persons = ko.observableArray(items);
+    this.personsToAdd = ko.observable("");
+
+    this.addPersons = function() {
+        if (this.personsToAdd() != "") {
+            this.persons([]);//Clears the list of persons
+            for(var i=0; i<this.personsToAdd();i++){
                 person.id=i;
-                this.items.push(person); // Adds the item. Writing to the "items" observableArray causes any associated UI to update.
+                this.persons.push(person); // Adds the item. Writing to the "items" observableArray causes any associated UI to update.
             }
-            this.itemToAdd("");// Clears the text box, because it's bound to the "itemToAdd" observable
+            this.personsToAdd("");// Clears the text box, because it's bound to the "itemToAdd" observable
         }
     }.bind(this);  // Ensure that "this" is always this view model
 
