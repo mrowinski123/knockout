8a3a87f59d8e865159da2f65bf28775d9164e892
Maciej Rowiński <mrowinski@it-dev.pl>
2016-12-15 10:53:00 +0100

    upadated for using observableArray and showing it

--- knockout.js
+++ knockout.js
@@ -1,91 +1,21 @@
-/**
- * Created by mrowinski on 14.12.2016.
- */
-var data={}; //container for objects
-var temp;
-var record="";
-var viewModel;
-function MyViewModel() {
-    this.size = ko.observable("3");
-    temp = this.size();
-    this.fullText = ko.computed(function() {
-        if (this.size != 0) {
-            console.log(temp);
-           /* for (var i = 0; i < temp; i++) {
-                data['Person ' + i] = {
-                    'id': i,
-                    'name': "JohnDoe"
-                };
+var SimpleListModel = function(items) {
+    var person={
+        id: "",
+        name: "JohnDoe"
+    }
+    this.items = ko.observableArray(items);
+    this.itemToAdd = ko.observable("");
+    this.addItem = function() {
+        if (this.itemToAdd() != "") {
+            this.items([]);//Clears the list of persons
+            for(var i=0; i<this.itemToAdd();i++){
+                person.id=i;
+                this.items.push(person); // Adds the item. Writing to the "items" observableArray causes any associated UI to update.
+            }
+            this.itemToAdd("");// Clears the text box, because it's bound to the "itemToAdd" observable
+        }
+    }.bind(this);  // Ensure that "this" is always this view model
 
-            }
-            for (var i in data) {
-                var subObj = data[i];
-                var buffor = [];
-                record += "ID: " + subObj['id'] + " | ";
-                record += "Name: " + subObj['name']+" || ";
-                buffor.push(record);
-            }
-        }
-        else
-            alert("dupa");
-        return record;**/
-        return FillArray();
-        }
-    }, MyViewModel);
 };
 
-
-function FillArray(){
-    for (var i = 0; i < temp; i++) {
-        data['Person ' + i]={
-            'id': i,
-            'name': "JohnDoe"
-        };
-
-    }
-    var buffor = [];
-    for (var i in data) {
-        var subObj = data[i];
-        record += "ID: " + subObj['id'] + " | ";
-        record += "Name: " + subObj['name']+" || ";
-        buffor.push(record);
-        record="";
-    }
- $("#new").append(buffor);
-    return buffor;
-}
-
-function knockoutLoad()
-{
-    viewModel = new MyViewModel();
-    ko.applyBindings(viewModel);//function to activate knockout
-};
-
-
-$(document).ready(function(){
-    $("#button").click(FillArray());
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ko.applyBindings(new SimpleListModel([]));
